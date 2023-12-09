63308fbb178df04cb4c87441b0b2e73e4a93b8be
nika <mn031621019@gmail.com>
2023-12-10 05:39:31 +0900

    디테일 화면 구성중

--- app/src/main/java/com/example/finalapp/MealDetailActivity.java
+++ app/src/main/java/com/example/finalapp/MealDetailActivity.java
@@ -1,12 +1,12 @@
 package com.example.finalapp;
+// MealDetailActivity.java
 
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
@@ -14,40 +14,54 @@ import java.util.Locale;
 
 public class MealDetailActivity extends AppCompatActivity {
 
+    // 각 항목에 대한 TextView 선언
+    private TextView textViewPlace;
+    private ImageView imageViewMeal;
+    private TextView textViewMenuName;
+    private TextView textViewRating;
+    private TextView textViewTime;
+    private TextView textViewCost;
+    private TextView textViewCalories;
+    private TextView textViewType;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_detail);
+
+        // 각 항목에 대한 TextView 초기화
+        textViewPlace = findViewById(R.id.textViewPlace);
+        imageViewMeal = findViewById(R.id.imageViewMeal);
+        textViewMenuName = findViewById(R.id.textViewMenuName);
+        textViewRating = findViewById(R.id.textViewRating);
+        textViewTime = findViewById(R.id.textViewTime);
+        textViewCost = findViewById(R.id.textViewCost);
+        textViewCalories = findViewById(R.id.textViewCalories);
+        textViewType = findViewById(R.id.textViewType);
 
         // Intent에서 Meal 객체 가져오기
         Intent intent = getIntent();
         if (intent.hasExtra("meal")) {
             Meal meal = (Meal) intent.getSerializableExtra("meal");
-            if (meal != null) {
-                // 뷰 초기화
-                ImageView imageView = findViewById(R.id.detailImageView);
-                TextView menuNameTextView = findViewById(R.id.detailMenuNameTextView);
-                TextView dateTextView = findViewById(R.id.detailDateTextView);
-                // 나머지 뷰 초기화 (필요한 만큼 추가)
 
-                // 이미지 설정 (byte[]에서 이미지로 변환하는 로직이 필요함)
-                // imageView.setImageBitmap(convertByteArrayToBitmap(meal.getImageBlob()));
+            // 각 TextView에 데이터 설정
+            textViewPlace.setText("Place: " + meal.getPlace());
 
-                // 나머지 데이터 설정
-                menuNameTextView.setText(meal.getMenuName());
-
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
-                String formattedDate = sdf.format(meal.getTime());
-                dateTextView.setText(formattedDate);
-                // 나머지 데이터 설정 (필요한 만큼 추가)
+            // 이미지 설정
+            byte[] imageBlob = meal.getImageBlob();
+            if (imageBlob != null && imageBlob.length > 0) {
+                imageViewMeal.setImageBitmap(BitmapFactory.decodeByteArray(imageBlob, 0, imageBlob.length));
+            } else {
+                // 이미지가 없는 경우 기본 이미지 설정
+                imageViewMeal.setImageResource(R.drawable.default_image);
             }
+
+            textViewMenuName.setText("Menu Name: " + meal.getMenuName());
+            textViewRating.setText("Rating: " + meal.getRating());
+            textViewTime.setText("Time: " + new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(meal.getTime()));
+            textViewCost.setText("Cost: " + meal.getCost());
+            textViewCalories.setText("Calories: " + meal.getCalories());
+            textViewType.setText("Type: " + meal.getType());
         }
     }
-
-    // byte[]를 Bitmap으로 변환하는 메서드 (필요하다면 추가 구현)
-    private Bitmap convertByteArrayToBitmap(byte[] byteArray) {
-        // ...
-        return null;
-    }
 }
-
