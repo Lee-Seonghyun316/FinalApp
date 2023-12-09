51da9d1ea3bdab71761b0c493ea7a0839b3bf0db
nika <mn031621019@gmail.com>
2023-12-10 06:23:28 +0900

    ui 조정

--- app/src/main/java/com/example/finalapp/MealListAdapter.java
+++ app/src/main/java/com/example/finalapp/MealListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.finalapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +22,11 @@ public class MealListAdapter extends ArrayAdapter<Meal> {
 
     private final Context context;
     private final ArrayList<Meal> mealList;
+    // 체크된 아이템들의 칼로리 합계를 계산하는 변수
+    private int totalCalories = 0;
+
+    // 체크된 아이템의 position을 저장하는 리스트
+    private ArrayList<Integer> checkedItems = new ArrayList<>();
 
     public MealListAdapter(Context context, ArrayList<Meal> mealList) {
         super(context, R.layout.meal_list_item, mealList);
@@ -76,10 +83,31 @@ public class MealListAdapter extends ArrayAdapter<Meal> {
             }
         });
 
+        // 체크박스 설정
+        CheckBox checkBox = rowView.findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // 체크 상태가 변경되었을 때의 처리
+            if (isChecked) {
+                // 체크된 경우
+                checkedItems.add(position); // 체크된 아이템의 position을 리스트에 추가
+                totalCalories += meal.getCalories(); // 해당 아이템의 칼로리를 합계에 추가
+            } else {
+                // 체크가 해제된 경우
+                checkedItems.remove((Integer) position); // 체크가 해제된 아이템의 position을 리스트에서 제거
+                totalCalories -= meal.getCalories(); // 해당 아이템의 칼로리를 합계에서 빼기
+            }
+            // 합계를 TextView에 업데이트
+            updateTotalCalories();
+        });
+
         return rowView;
     }
 
-// ...
+    // 체크된 아이템들의 칼로리 합계를 업데이트하고 TextView에 표시
+    private void updateTotalCalories() {
+        TextView totalCaloriesTextView = ((Activity) context).findViewById(R.id.totalCaloriesTextView);
+        totalCaloriesTextView.setText("Total Calories: " + totalCalories);
+    }
 
 
     // byte[]를 Bitmap으로 변환하는 메서드 (필요하다면 추가 구현)
